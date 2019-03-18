b5e6bc1d96c058d28736c6f648682f227ee29822
oinkoinkduck <sscw0630@gmail.com>
2019-03-18 14:27:54 +0900

    Initial Commit

---
+++ capstone_3/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  capstone_3
+//
+//  Created by parkyehyun on 2019. 3. 18..
+//  Copyright © 2019년 parkyehyun. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
